6978a83bc5f1339e244b8297c1a5f2e8ae921e3f
Akash007sharma16 <121238769+Akash007sharma16@users.noreply.github.com>
2026-01-19 14:43:55 +0530

    Add character filtering logic to Program.cs
    Implement functionality to remove characters from s1 that are present in s2, ignoring case and duplicates.

--- TopBrainPractice1/Program.cs
+++ TopBrainPractice1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string s1=Console.ReadLine();
+        string s2=Console.ReadLine();
+        HashSet<char> hs=new HashSet<char>();
+        foreach(var c in s2) hs.Add(char.ToLower(c));
+        StringBuilder str=new StringBuilder();
+        foreach(char i in s1)
+        {
+            char lower=char.ToLower(i);
+            if(!hs.Contains(lower)) str.Append(lower);
+        }
+        StringBuilder res=new StringBuilder();
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (i == 0 || str[i] != str[i - 1])
+            {
+                res.Append(str[i]);
+            }
+        }
+        Console.WriteLine(res.ToString());
+    }
+}
